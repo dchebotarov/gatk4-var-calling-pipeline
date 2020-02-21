d854d05c7e2cb6803ac8c44ba77ca4c5a97c7d81
Dmytro Chebotarov <d.chebotarov@irri.org>
2020-02-22 00:01:42 +0800

    change file matching

--- makeScriptsAll.sh
+++ makeScriptsAll.sh
@@ -26,8 +26,7 @@ ref=$3
 
 while read -r in1 in2 remainder 
 do 
-
-	# Find actual files
+	# Find actual files (@deprecate; use table sample unit read1path read2path)
 	rd1=`find $DATADIR -name $in1`
 	rd2=`find $DATADIR -name $in2`
 
@@ -35,22 +34,25 @@ do
 		echo Could not find fastq file $in1 in $DATADIR  
 		continue
 	fi 
-
+	echo rd1=$rd1 rd2=$rd2
+	
 	# Extract Sample ID - dataset specific. Edit the file extensions to remove
-	prefix=${in1%_1.clean.fq.gz}
+	prefix=${in1%1.fq*} # edit 
 	SID=${prefix##*/}
-	sample=${SID%%_*}
+	sample=${SID%_*}
+	echo "# SAMPLE: $sample "
 
 	# Create a subdirectory equal to sample name
 	commanddir=$sample
 	mkdir -p $commanddir
+	mkdir -p output/$sample
+
 	# Create per sample SLURM files
-    ./perSampleScripts.sh $sample  $rd1  $rd2    \
-      output/$sample  \
-      $ref
+    ./perSampleScripts.sh "$sample"  "$rd1"  "$rd2" "output/$sample" "$ref"
 
 	# Create "aggregated" SLURM files, for easier submission (at the price of same configuration for all job steps, which is suboptimal)
 	echo "cat aln-${sample}.sl fixmate-${sample}.sl markdup-${sample}.sl addrep-${sample}.sl haplotypecaller-${sample}.sl  > all-${sample}.sl " | sh
+	echo "cat aln-${sample}.sl fixmate-${sample}.sl markdup-${sample}.sl addrep-${sample}.sl   > fq2bam-${sample}.sl " | sh
 	echo "cat  fixmate-${sample}.sl markdup-${sample}.sl addrep-${sample}.sl haplotypecaller-${sample}.sl  cleanup-${sample}.sl  > after-aln-${sample}.sl " | sh
 	# Move all scripts into the directory for the sample
 	echo "mv *-${sample}.sl  $commanddir"  | sh
